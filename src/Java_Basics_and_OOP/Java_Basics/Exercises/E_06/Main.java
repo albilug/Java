25f15593e98d8e07db780dbede82dca4cfa6f1a4
albilug <alberto.lugato@gmail.com>
2023-02-09 21:16:16 +0100

    aggiunto E_06

---
+++ src/Java_Basics_and_OOP/Java_Basics/Exercises/E_06/Main.java
@@ -0,0 +1,88 @@
+package Java_Basics_and_OOP.Java_Basics.Exercises.E_06;
+
+public class Main {
+    // Don't forget to use comments to clear things up if you need them!
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4Bonus();
+    }
+
+    /**
+     * 1: Create a double variable called value, create another double variable called valueSquareRoot
+     *
+     *    Use Math.sqrt to find the square root of value and assign it to valueSquareRoot
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        double value = 0.99;
+        double valueSquareRoot = Math.sqrt(value);
+        System.out.println(valueSquareRoot);
+        // Write your code here
+    }
+
+    /**
+     * 2: See the code below
+     *
+     *    Print out the highest value using Math.max()
+     *
+     *    Then print out the lowest value using Math.min()
+     *
+     *    You must put your variables into these methods and separate them with a comma
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        int valueA = 5;
+        int valueB = 10;
+
+        System.out.println("Max=" + Math.max(valueA, valueB));
+        System.out.println("Min=" + Math.min(valueA, valueB));
+
+        // Write your code here
+    }
+
+    /**
+     * 3: Use Modulus (%) to calculate the remaining amount of money if we buy as many burgers as possible, print remainder
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+
+        int totalMoney = 50;
+        int costPerBurger = 9;
+        // Write your code here
+        int moneyLeftAfterMaxBurger = totalMoney % costPerBurger;
+        System.out.println("Remainder=" + moneyLeftAfterMaxBurger);
+
+
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * I've chosen loads of methods from the Math Library, write comments above each line to guess what they do!
+     *
+     * You do not need to write any code
+     *
+     * Answers can be found in resources
+     */
+    private static void exercise4Bonus() {
+        System.out.println("\nExercise 4 (Bonus!):");
+
+        double ourValue = 4.5;
+
+        // Replace with your comment 1
+        System.out.println(ourValue + " after using ceiling=" + Math.ceil(ourValue)); //Int arrotondato per eccesso.
+        // Replace with your comment 2
+        System.out.println(ourValue + " after using floor=" + Math.floor(ourValue)); //Int arrotondato per difetto.
+        // Replace with your comment 3
+        System.out.println(ourValue + " after using round=" + Math.round(ourValue)); //Arrotonda: se >=.5, per eccesso. Se <.5, per difetto.
+        // Replace with your comment 4
+        System.out.println(ourValue + " after using powerOf with the value of 2=" + Math.pow(ourValue, 2)); //eleva a esponente 2.
+        // Replace with your comment 5
+        System.out.println(ourValue + " after using powerOf with the value of 3=" + Math.pow(ourValue, 3)); //eleva a esponente 3.
+        // Replace with your comment 6
+        System.out.println(ourValue + " times random =" + Math.random() * ourValue); //moltiplica ourValue per un numero casuale tra 0 e 1.
+    }
+}
+
